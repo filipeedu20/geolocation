23eb2c8ca55f401a55472942d3aefd4ad2282f4d
Filipe Eduardo <filipeedu.bh@gmail.comm>
2019-07-10 18:03:30 -0300

    Adicionando integração com Google maps.

--- js/geolocalizacao.js
+++ js/geolocalizacao.js
@@ -1,4 +1,7 @@
 const x = document.getElementById("demo");
+var map ;
+
+
 
 function getLocation() {
 	if (navigator.geolocation) {
@@ -45,4 +48,58 @@ function showPosition(position) {
 	 console.log(NovoPonto.getLatitude().length);
 	 console.log(NovoPonto.getLongitude().length);
 }
+		
 
+
+function loadLocations(){
+	pontos = [
+		{
+			"Latitude": -19.918178,
+			"Longitude": -43.942461
+		},
+		
+		{
+			"Latitude": -19.920393,
+			"Longitude": -43.944180
+		},
+		{
+			"Latitude": -19.915134,
+			"Longitude": -43.947737
+		},
+		{
+			"Latitude": -19.904994,
+			"Longitude": -43.898574
+		}
+
+	];
+
+	$.each(pontos, function(index, pontos) {		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(pontos.Latitude, pontos.Longitude),
+			title: "Ponto marcado",
+			icon: 'img/flag.png',	
+			map: map
+		});
+	});
+}
+// Inicia mapa 
+function initMap() {
+	
+	var latitude = -19.919086;
+	var longitude =   -43.938810;
+	
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: latitude, lng:  longitude},				
+	zoom: 16
+	});
+
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(latitude, longitude),
+		title: "Localização",
+		map: map,
+		icon: 'img/location.png'	
+	});
+
+	// abre pontos marcados no mapa 
+	loadLocations();
+}
